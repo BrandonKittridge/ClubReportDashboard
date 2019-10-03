58854415ae6cf3f76faecbf1c9e495a02ecca4c5
peter petrucci <petrucci209@gmail.com>
2019-10-03 11:54:46 -0400

    Monthly Report Page test 1

--- ClubReportDashboard/ClubReportDashboard/Controllers/AdminController.cs
+++ ClubReportDashboard/ClubReportDashboard/Controllers/AdminController.cs
@@ -25,5 +25,10 @@ namespace ClubReportDashboard.Controllers
         {
             return View();
         }
+
+        public IActionResult MonthlyReport()
+        {
+            return View();
+        }
     }
 }
