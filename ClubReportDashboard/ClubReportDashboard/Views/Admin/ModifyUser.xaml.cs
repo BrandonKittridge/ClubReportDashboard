85b2acf9d26e02140110ad6debc560a22481d9dc
Brandon Kittridge <n01039829@unf.edu>
2020-01-27 13:03:49 -0500

    Added some user views I had already made that make use of the user models

---
+++ ClubReportDashboard/ClubReportDashboard/Views/Admin/ModifyUser.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ClubReportDashboard.Views.Admin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ModifyUser : ContentPage
+    {
+        public ModifyUser()
+        {
+            InitializeComponent();
+        }
+    }
+}
